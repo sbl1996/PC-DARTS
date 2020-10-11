6433d8c22a3fedeff2242a38764e85bb0116250c
sbl1996 <sbl1996@126.com>
2020-10-11 16:40:05 +0800

    Add train_search2

--- architect.py
+++ architect.py
@@ -4,8 +4,6 @@ import torch
 class Architect(object):
 
   def __init__(self, model, args):
-    self.network_momentum = args.momentum
-    self.network_weight_decay = args.weight_decay
     self.model = model
     self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
         lr=args.arch_learning_rate, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
--- train.py
+++ train.py
@@ -126,7 +126,7 @@ def train(train_queue, model, criterion, optimizer):
 
   for step, (input, target) in enumerate(train_queue):
     input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
+    target = Variable(target).cuda()
 
     optimizer.zero_grad()
     logits, logits_aux = model(input)
@@ -158,7 +158,7 @@ def infer(valid_queue, model, criterion):
 
   for step, (input, target) in enumerate(valid_queue):
     input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    target = Variable(target, volatile=True).cuda()
 
     logits, _ = model(input)
     loss = criterion(logits, target)
