73f053563bd51cd90c86c5c448ab2b468141dd69
sbl1996 <sbl1996@126.com>
2020-10-16 17:05:32 +0800

    Add FTSO to genotype

--- genotypes.py
+++ genotypes.py
@@ -81,3 +81,20 @@ PC_DARTS_image = Genotype(normal=[('skip_connect', 1), ('sep_conv_3x3', 0), ('se
 
 PCDARTS = PC_DARTS_cifar
 
+FTSO = Genotype(
+    normal=[
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1)
+    ],
+    normal_concat=[2, 3, 4, 5],
+    reduce=[
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+        ('sep_conv_3x3', 0), ('sep_conv_3x3', 1)
+    ],
+    reduce_concat=[2, 3, 4, 5]
+)
+
